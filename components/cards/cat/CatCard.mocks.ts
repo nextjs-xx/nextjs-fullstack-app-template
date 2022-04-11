311e7d37fc0bbae6a3ae37756424b51592ed3bf9
liliang8858 <liliang8858@126.com>
2022-04-11 20:34:42 +0800

    add cat card

--- components/cards/cat/CatCard.mocks.ts
+++ components/cards/cat/CatCard.mocks.ts
@@ -1,7 +1,11 @@
 import { ICatCard } from './CatCard';
 
 const base: ICatCard = {
-  sampleTextProp: 'Hello world!',
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Alex',
+  time: '2h ago',
 };
 
 export const mockCatCardProps = {
