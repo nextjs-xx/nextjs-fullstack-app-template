ab7bb749eadd118aa8377a1e7be792664c6963e8
liliang8858 <liliang8858@126.com>
2022-04-11 19:30:35 +0800

    add templates

---
+++ components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,10 @@
+import styles from './BaseTemplate.module.css';
+export interface IBaseTemplate {
+  sampleTextProp: string;
+}
+
+const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
+  return <div className={styles.component}>{sampleTextProp}</div>;
+};
+
+export default BaseTemplate;
