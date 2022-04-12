5f4261106d49ed649e7f4e429c0573fa4e908caa
liliang8858 <liliang8858@126.com>
2022-04-12 18:06:57 +0800

    add index

--- components/layouts/sidebar/SidebarLayout.tsx
+++ components/layouts/sidebar/SidebarLayout.tsx
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import styles from './SidebarLayout.module.css';
-export interface ISidebarLayout {
-  sampleTextProp: string;
-}
 
-const SidebarLayout: React.FC<ISidebarLayout> = ({ sampleTextProp }) => {
-  return <div className={styles.component}>{sampleTextProp}</div>;
+export interface ISidebarLayout {}
+
+const SidebarLayout: React.FC<ISidebarLayout> = () => {
+  return (
+    <nav className={styles.nav}>
+      <input className={styles.input} placeholder="Search..." />
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact</a>
+      </Link>
+    </nav>
+  );
 };
 
 export default SidebarLayout;
