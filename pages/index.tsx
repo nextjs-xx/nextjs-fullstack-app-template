67e08dbea3a772ecb2df5722b1636a5aae11a8c0
liliang8858 <liliang8858@126.com>
2022-05-02 13:16:50 +0800

    update use avatar default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import ButtonTemplate from '../components/nextui/button/ButtonTemplate';
-import { mockButtonTemplateProps } from '../components/nextui/button/ButtonTemplate.mocks';
 import CatCard from '../components/cards/cat/CatCard';
 import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import AvatarUI from '../components/nextui/avatar/AvatarUI';
+import { mockAvatarProps } from '../components/nextui/avatar/AvatarUI.mocks';
+import ButtonTemplate from '../components/nextui/button/ButtonTemplate';
+import { mockButtonTemplateProps } from '../components/nextui/button/ButtonTemplate.mocks';
 import styles from '../styles/Home.module.css';
 import { NextPageWithLayout } from './page';
 
@@ -13,6 +15,7 @@ const Home: NextPageWithLayout = () => {
       <h1 className={styles.title}>{mockCatCardProps.base.title}</h1>
       <CatCard {...mockCatCardProps.base} />
       <ButtonTemplate {...mockButtonTemplateProps.base} />
+      <AvatarUI {...mockAvatarProps.base} />
     </section>
   );
 };
