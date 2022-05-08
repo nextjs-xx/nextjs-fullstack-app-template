b82be79f6a097181bf8d3118037b571ce96454f9
liliang8858 <liliang8858@126.com>
2022-05-08 21:02:44 +0800

    add twitter

--- twitter-2-yt-clone/components/Widgets.tsx
+++ twitter-2-yt-clone/components/Widgets.tsx
@@ -1,14 +1,16 @@
 import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div>
+    <div className='px-2 mt-2 '>
         {/* search */}
-        <div className='flex items-center space-x-2 bg-gray-100'>
-            <SearchIcon className='h-5 w-5'/>
-            <input type="text" placeholder="Search Twitter"/>
+        <div className='flex items-center space-x-2 rounded-full bg-gray-100 p-3'>
+            <SearchIcon className='h-5 w-5 text-gray-400'/>
+            <input type="text" placeholder="Search Twitter" className='flex-1 outline-none bg-transparent'/>
         </div>
+        <TwitterTimelineEmbed sourceType='profile' screenName='saurabhnemade' options={{height:400}}/>
     </div>
   )
 }
