bbeeec2a79475f48381e375e0f8de8c023f537af
liliang8858 <liliang8858@126.com>
2022-05-08 21:37:05 +0800

    update

--- twitter-2-yt-clone/components/Sidebar.tsx
+++ twitter-2-yt-clone/components/Sidebar.tsx
@@ -13,7 +13,7 @@ import SideBarRow from './SideBarRow'
 
 function Sidebar() {
     return (
-        <div className='col-span-2 flex flex-col '>
+        <div className='col-span-2 items-center px-4 md:items-start  flex flex-col '>
             <img className='h-10 w-10' src="https://links.papareact.com/drq" alt="" srcset="" />
             <SideBarRow Icon={HomeIcon} title="Home"/>
             <SideBarRow Icon={HashtagIcon} title="Explore"/>
--- twitter-2-yt-clone/components/Widgets.tsx
+++ twitter-2-yt-clone/components/Widgets.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div className='px-2 mt-2 col-span-2'>
+    <div className='px-2 mt-2 col-span-2 hidden lg:inline'>
         {/* search */}
         <div className='flex items-center space-x-2 rounded-full bg-gray-100 p-3'>
             <SearchIcon className='h-5 w-5 text-gray-400'/>
